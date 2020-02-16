f953fd7a94b264ca9bc25d4f491b23573b36b661
Kyle Decot <kyle@joinroot.com>
2020-02-16 13:48:01 -0500

    CLI Flag Improvements

--- lib/hard_cider/cli.rb
+++ lib/hard_cider/cli.rb
@@ -7,11 +7,27 @@ module HardCider
   class CLI
     extend GLI::App
 
+    program_desc 'Wait for Apple IPAs'
+
+    desc 'Wait for Apple IPAs'
     command :wait do |c|
-      c.flag %i[i issuer-id]
-      c.flag %i[k key-id]
-      c.flag %i[p private-key]
-      c.flag %i[b bundle-id]
+      c.flag %i[i issuer-id],
+             desc: 'App Store Connect API Issuer ID',
+             mask: true,
+             default_value: ENV['APP_STORE_CONNECT_ISSUER_ID']
+
+      c.flag %i[k key-id],
+             desc: 'App Store Connect API Key ID',
+             mask: true,
+             default_value: ENV['APP_STORE_CONNECT_KEY_ID']
+
+      c.flag %i[p private-key],
+             desc: 'App Store Connect API Private Key',
+             mask: true,
+             default_value: ENV['APP_STORE_CONNECT_PRIVATE_KEY']
+
+      c.flag %i[b bundle-id],
+             required: true
 
       c.action do |_global_options, options, _args|
         HardCider.wait(Utils.underscore_keys(options))
